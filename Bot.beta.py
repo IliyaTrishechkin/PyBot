64ec59549a6938f925734ad845879863d4f40fa4
Yaro-PY <yspersik@gmail.com>
2025-05-16 22:36:19 +0300

    Yaro

--- Bot.beta.py
+++ Bot.beta.py
@@ -1,8 +1,13 @@
 import logging
 import json
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, ContextTypes, CallbackQueryHandler, CommandHandler
+
+load_dotenv()
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -83,7 +88,10 @@ async def handle_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await query.edit_message_text(text=cat, reply_markup=InlineKeyboardMarkup(keyboard))
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token('7311053326:AAES8JRhk--Nz3fA_LcMjeMvMG5Pr5FfDys').build()
+    if not TOKEN:
+        raise ValueError("TELEGRAM_BOT_TOKEN не знайдено. Додай токен у .env файл.")
+
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler('start', start))
     app.add_handler(CallbackQueryHandler(handle_callback, pattern='^(cat|q|back)\|'))
     app.run_polling()
